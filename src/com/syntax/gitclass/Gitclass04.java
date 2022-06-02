f800ef758af6907f5b5c5be7cf2a0f05a535e905
cherry7859 <cmfinley97@gmail.com>
2022-06-01 20:31:19 -0500

    edited git class04

--- src/com/syntax/gitclass/Gitclass04.java
+++ src/com/syntax/gitclass/Gitclass04.java
@@ -4,5 +4,6 @@ public class Gitclass04 {
 public static void main (String []args) {
 	
 	System.out.println("Additional file to commit");
+	System.out.println("We are making changes the next morning");
 }
 }
